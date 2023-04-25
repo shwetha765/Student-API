5612deb40007fa088e10645f2ed9e5d4d764caa1
Siri <128618015+shwetha765@users.noreply.github.com>
2023-04-26 02:04:28 +0530

    Created student model

--- student_api/models.py
+++ student_api/models.py
@@ -5,4 +5,4 @@ class Student(models.Model):
     student_id = models.BigAutoField(primary_key=True)
     student_name = models.TextField(null=False)
     city = models.CharField(max_length=255)
-    contact = models.CharField(max_length=255)
+    contact = models.CharField(max_length=255) 
